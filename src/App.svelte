1cbd082ab8c50d6698b577707c1ee9d2d2acfdfd
kimdohee <ehgml1073@gmail.com>
2024-02-12 16:46:39 +0900

    svelte, vite.. . . 등등

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  // @ts-nocheck
+
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import NotFount from "./pages/NotFount.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/Login": Login,
+    "/Signup": Signup,
+    "/Write": Write,
+    "*": NotFount,
+  };
+</script>
+
+<Router {routes} />
+
+<style>
+</style>
