90313db11819b169f9ed24c149da3ab7035d0687
kimdohee <ehgml1073@gmail.com>
2024-02-13 23:09:54 +0900

    잘하구이땨

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
+
 const firebaseConfig = {
+  apiKey: "AIzaSyCGJxBPBT97eklr1C6-FmIOn9oHlTGaNuY",
+  authDomain: "carrot-market-dodo.firebaseapp.com",
   databaseURL:
-    "https://carrot-market-dodo-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  storageBucket: "gs://carrot-market-dodo.appspot.com",
+    "https://carrot-market-dodo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "carrot-market-dodo",
+  storageBucket: "carrot-market-dodo.appspot.com",
+  messagingSenderId: "164911656119",
+  appId: "1:164911656119:web:6d9247467741bc19977132",
+  measurementId: "G-92RR7601KH",
 };
 
 // Initialize Firebase
@@ -16,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
--- src/App.svelte
+++ src/App.svelte
@@ -8,17 +8,46 @@
   import Write from "./pages/Write.svelte";
   import Router from "svelte-spa-router";
   import "./css/style.css";
+  import { user$ } from "./store";
+  import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+  import MyPage from "./pages/MyPage.svelte";
+
+  let isLoding = true;
+
+  const auth = getAuth();
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token"); //토큰을 바탕으로
+    if (!token) return (isLoding = false);
+
+    const credential = GoogleAuthProvider.credential(null, token); //인증 진행
+    const result = await signInWithCredential(auth, credential); //결과값에서 유저 정보 있을 것
+    const user = result.user; //결과값에 유저정보 가져옴
+    user$.set(user); //스토어에 유저정보 업데이트
+    isLoding = false;
+  };
 
   const routes = {
     "/": Main,
-    "/Login": Login,
     "/Signup": Signup,
     "/Write": Write,
+    "/My": MyPage,
     "*": NotFount,
   };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
-
-<style>
-</style>
+{#if isLoding}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
